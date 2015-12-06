85fec16dfdd7a15f428d075dcad1604d1927b2e9
wreckloose <7andpiper@gmail.com>
2015-12-05 19:38:25 -0500

    Add hello_world action

---
+++ actions/hello_world.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @param Horseman phantomInstance
+ * @param string url
+ */
+module.exports = function (phantomInstance, url) {
+
+  if (!url || typeof url !== 'string') {
+    throw 'You must specify a url to ping';
+  }
+
+  console.log('Pinging url: ', url);
+
+  phantomInstance
+    .open(url)
+    .status()
+    .then(function (statusCode) {
+      if (Number(statusCode) >= 400) {
+        throw 'Page failed with status: ' + statusCode;
+      } else {
+        console.log('Hello world. Status code returned: ', statusCode);
+      }
+    })
+    .catch(function (err) {
+      console.log('Error: ', err);
+    })
+    .close(); // Always close the Horseman instance, or you might end up with orphaned phantom processes
+};
